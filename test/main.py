bc90f747ed26c3ce9825d2601041e8324cdba089
MJ <1105kmj@gmail.com>
2025-03-14 15:47:52 +0900

    agent 기능 통합
    main.py 파일 agent 테스트용 코드로 변경되었습니다.

--- test/main.py
+++ test/main.py
@@ -8,21 +8,40 @@ sys.path.append("C:/test")
 import streamlit as st
 
 from services.db_service import db_name, db_phone
+from services.line_send_message import send_line_message
 from services.llm_service import llm_fuc
 
 names=db_name()
 phones=db_phone()
 
+CHANNEL_ACCESS_TOKEN="U5mOtIoUbFJ5K9L1cZJ3bqiMEEbA/aRriHqEU4IEntdiu4D7Ncr+C5YwxWSzAYAnXVYTNCDSbaC+rQdrxO/Lsjv7/bXOaqyFBqxxRVJp2IDwFMd1VgIhFfU0UMXK2YlPBISylCrSCK5K+h1xDCXdKgdB04t89/1O/w1cDnyilFU="
+USER_ID="Uaecc6981aace6cd3c6788ffb6019f1ff" #Reciever
+
 def main():
 
     results=[]
-
     if st.button("전송 요청"):
         # db 개수만큼 반복 출력 포문 두개
         for name, phone in zip(names,phones):
-            result=llm_fuc(name,phone)
-            results.append(result)
-            st.write(f"이름:{name} 번호: {phone} 안내문: {result}")
+            if name=='youngho':
+                result=llm_fuc(name,phone)
+                results.append(result)
+                st.write(f"이름:{name} 번호: {phone} 안내문: {result}")
+                message=result
+                send_line_message(CHANNEL_ACCESS_TOKEN, USER_ID, message)
+
+
+#gemini test 버전 생성을 위해 주석
+#    if st.button("전송 요청"):
+#        # db 개수만큼 반복 출력 포문 두개
+#        for name, phone in zip(names,phones):
+#            result=llm_fuc(name,phone)
+#            results.append(result)
+#            st.write(f"이름:{name} 번호: {phone} 안내문: {result}")
+#            if name=='youngho': 
+#                send_line_message(channel_access_token,user_)    
+        
+
 
 if __name__=="__main__":
     main()
---
+++ test/services/line_send_message.py
@@ -0,0 +1,45 @@
+#Module
+import json
+import os
+import sys
+
+import requests
+
+# 절대 경로
+
+
+sys.path.append("C:/test")
+
+# LINE 메시지 보내기 함수
+def send_line_message(channel_access_token, user_to_send_id, message):
+    url = 'https://api.line.me/v2/bot/message/push'
+    
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {channel_access_token}'
+        #'X-Line-Retry-Key':'Uaecc6981aace6cd3c6788ffb6019f1ff'
+    }
+    
+    data = {
+        'to': user_to_send_id,
+        'messages': [
+            {
+                'type': 'text',
+                'text': message
+            }
+        ]
+    }
+    
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response.text)
+    print(response.status_code)
+    
+    return response
+
+#.env환경설정
+CHANNEL_ACCESS_TOKEN="U5mOtIoUbFJ5K9L1cZJ3bqiMEEbA/aRriHqEU4IEntdiu4D7Ncr+C5YwxWSzAYAnXVYTNCDSbaC+rQdrxO/Lsjv7/bXOaqyFBqxxRVJp2IDwFMd1VgIhFfU0UMXK2YlPBISylCrSCK5K+h1xDCXdKgdB04t89/1O/w1cDnyilFU="
+USER_ID="Uaecc6981aace6cd3c6788ffb6019f1ff" #Reciever
+
+
+message="안녕하세요! LINE 메시지 테스트입니다."
+send_line_message(CHANNEL_ACCESS_TOKEN, USER_ID, message)
